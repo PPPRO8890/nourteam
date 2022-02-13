b617adb44182cb13fad32e8834aff54637cdf4f3
PPPRO8890 <86791918+PPPRO8890@users.noreply.github.com>
2022-02-13 17:16:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var n1=document.getElementsByClassName("nityaInto")[0]
+var n2=document.getElementsByClassName("TahaInto")[0]
+
+function Nityamam(){
+n1.style.display="block"
+n2.style.display="none"
+}
+
+function Zaidisir(){
+    n1.style.display="none"
+    n2.style.display="block"
+    }
